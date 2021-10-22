8b3ac94d74dfb067763b6ac0510cc081bb4aa95e
陈小斌 <chenxiaobin@chenxiaobindeMacBook-Pro.local>
2021-10-22 14:56:21 +0800

    fix: 修复navbar

--- docs/.vuepress/configs/navbar/zh.ts
+++ docs/.vuepress/configs/navbar/zh.ts
@@ -2,14 +2,14 @@ import type { NavbarConfig } from '@vuepress/theme-default'
 
 export const zh: NavbarConfig = [
   {
-    text: 'Foo',
-    link: '/zh/foo/',
-  },
-  {
-    text: 'Group',
+    text: '目录',
     children: [{
-      text: "xxx",
-      link: "/zh/group/foo.md"
+      text: "start1",
+      link: "/start/01_start1.md"
+    },
+    {
+      text: "start2",
+      link: "/start/02_start2.md"
     }
   ]},
 ];
