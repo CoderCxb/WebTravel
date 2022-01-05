886963c3ae3d3b90f1af75f7c385d4122f610070
cxb <chenxiaobin_cxb@youzan.com>
2022-01-05 15:56:16 +0800

    🐛 Bug Fixes                | Bug修复(CI流程/项目构建): 自动化爬虫更新搜索功能

---
+++ scripts/search.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+APPLICATION_ID=$1
+API_KEY=$2
+INDEX_NAME=$3
+START_URLS=$4
+
+git clone https://github.com/cxblovecw/docsearch-scraper
+cd docsearch-scraper
+yarn set-up $APPLICATION_ID $API_KEY $INDEX_NAME $START_URLS
+pipenv run make search
